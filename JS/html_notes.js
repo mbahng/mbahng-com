188bcc06e59990a138e47f365adacdd8e27f25e8
Muchang Bahng <muchang.bahng@duke.edu>
2022-05-04 17:07:53 +0900

    first commit

---
+++ JS/html_notes.js
@@ -0,0 +1,175 @@
+function show_hide_1() {
+    var x = document.getElementById("section_content_1");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+      document.getElementById("show_hide_1").innerHTML = "[Hide]";
+    } else {
+      x.style.display = "none";
+      document.getElementById("show_hide_1").innerHTML = "[Show]";
+    }
+  }
+
+function show_hide_2() {
+    var x = document.getElementById("section_content_2");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+      document.getElementById("show_hide_2").innerHTML = "[Hide]";
+    } else {
+      x.style.display = "none";
+      document.getElementById("show_hide_2").innerHTML = "[Show]";
+    }
+  }
+
+function show_hide_3() {
+    var x = document.getElementById("section_content_3");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+      document.getElementById("show_hide_3").innerHTML = "[Hide]";
+    } else {
+      x.style.display = "none";
+      document.getElementById("show_hide_3").innerHTML = "[Show]";
+    }
+  }
+
+  function show_hide_4() {
+    var x = document.getElementById("section_content_4");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+      document.getElementById("show_hide_4").innerHTML = "[Hide]";
+    } else {
+      x.style.display = "none";
+      document.getElementById("show_hide_4").innerHTML = "[Show]";
+    }
+  }
+
+  function show_hide_5() {
+    var x = document.getElementById("section_content_5");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+      document.getElementById("show_hide_5").innerHTML = "[Hide]";
+    } else {
+      x.style.display = "none";
+      document.getElementById("show_hide_5").innerHTML = "[Show]";
+    }
+  }
+
+  function show_hide_6() {
+    var x = document.getElementById("section_content_6");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+      document.getElementById("show_hide_6").innerHTML = "[Hide]";
+    } else {
+      x.style.display = "none";
+      document.getElementById("show_hide_6").innerHTML = "[Show]";
+    }
+  }
+
+  function show_hide_7() {
+    var x = document.getElementById("section_content_7");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+      document.getElementById("show_hide_7").innerHTML = "[Hide]";
+    } else {
+      x.style.display = "none";
+      document.getElementById("show_hide_7").innerHTML = "[Show]";
+    }
+  }
+
+  function show_hide_8() {
+    var x = document.getElementById("section_content_8");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+      document.getElementById("show_hide_8").innerHTML = "[Hide]";
+    } else {
+      x.style.display = "none";
+      document.getElementById("show_hide_8").innerHTML = "[Show]";
+    }
+  }
+
+  function show_hide_9() {
+    var x = document.getElementById("section_content_9");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+      document.getElementById("show_hide_9").innerHTML = "[Hide]";
+    } else {
+      x.style.display = "none";
+      document.getElementById("show_hide_9").innerHTML = "[Show]";
+    }
+  }
+
+  function show_hide_10() {
+    var x = document.getElementById("section_content_10");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+      document.getElementById("show_hide_10").innerHTML = "[Hide]";
+    } else {
+      x.style.display = "none";
+      document.getElementById("show_hide_10").innerHTML = "[Show]";
+    }
+  }
+
+  function show_hide_11() {
+    var x = document.getElementById("section_content_11");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+      document.getElementById("show_hide_11").innerHTML = "[Hide]";
+    } else {
+      x.style.display = "none";
+      document.getElementById("show_hide_11").innerHTML = "[Show]";
+    }
+  }
+
+  function show_hide_12() {
+    var x = document.getElementById("section_content_12");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+      document.getElementById("show_hide_12").innerHTML = "[Hide]";
+    } else {
+      x.style.display = "none";
+      document.getElementById("show_hide_12").innerHTML = "[Show]";
+    }
+  }
+
+  function show_hide_13() {
+    var x = document.getElementById("section_content_13");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+      document.getElementById("show_hide_13").innerHTML = "[Hide]";
+    } else {
+      x.style.display = "none";
+      document.getElementById("show_hide_13").innerHTML = "[Show]";
+    }
+  }
+
+  function show_hide_14() {
+    var x = document.getElementById("section_content_14");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+      document.getElementById("show_hide_14").innerHTML = "[Hide]";
+    } else {
+      x.style.display = "none";
+      document.getElementById("show_hide_14").innerHTML = "[Show]";
+    }
+  }
+
+  function show_hide_15() {
+    var x = document.getElementById("section_content_15");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+      document.getElementById("show_hide_15").innerHTML = "[Hide]";
+    } else {
+      x.style.display = "none";
+      document.getElementById("show_hide_15").innerHTML = "[Show]";
+    }
+  }
+
+  function show_hide_16() {
+    var x = document.getElementById("section_content_16");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+      document.getElementById("show_hide_16").innerHTML = "[Hide]";
+    } else {
+      x.style.display = "none";
+      document.getElementById("show_hide_16").innerHTML = "[Show]";
+    }
+  }
